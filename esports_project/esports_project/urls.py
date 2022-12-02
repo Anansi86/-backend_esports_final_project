6b2c54d52f878011bf1d77525fcddfa4eb29ea41
Anansi86 <shawn.staton@student.awesomeinc.org>
2022-12-02 18:44:10 +0000

    I got tokensgit add --all!

--- esports_project/esports_project/urls.py
+++ esports_project/esports_project/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('esports_app.url')),
+    path('api/', include('esports_app.url'))
 ]
 
