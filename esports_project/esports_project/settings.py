ddd8227ca6b2d9fda91c8b7b0d5c0795d50e454d
Anansi86 <shawn.staton@student.awesomeinc.org>
2022-12-02 20:12:54 +0000

    create user

--- esports_project/esports_project/settings.py
+++ esports_project/esports_project/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "corsheaders",
     'rest_framework',
+    'rest_framework.authtoken',
     'esports_app',
 ]
 
@@ -139,7 +140,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': False,
